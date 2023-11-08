33b5ec90784d9c94befd7f0724d410cc60cc800f
Marcos Ariel Rodrigues de Jesus <marcosariel.rj@hotmail.com>
2023-11-08 10:37:13 -0300

    Alteração Desafio 2 função de nível

--- Desafio 2.js
+++ Desafio 2.js
@@ -1,44 +1,49 @@
-function calcRanking (vitorias, derrotas){
+function calcRanking(vitorias, derrotas) {
     return vitorias - derrotas
 }
 
-let ranking = calcRanking(30, 10)
-let nivel
+let ranking = calcRanking(85, 20)
+let nivel = calcNivel(ranking)
 
 
-if (ranking <= 10) {
-    nivel = "Ferro"
+function calcNivel(ranking) {
 
+    let result 
+
+    if (ranking <= 10) {
+        nivel = "Ferro"
+
+    }
+
+    else if (ranking <= 20 && ranking > 10) {
+        result = "Bronze"
+
+    }
+
+    else if (ranking <= 50 && ranking > 20) {
+        result = "Prata"
+
+    }
+
+    else if (ranking <= 80 && ranking > 50) {
+        result = "Ouro"
+
+    }
+
+    else if (ranking <= 90 && ranking > 80) {
+        result = "Diamante"
+
+    }
+
+    else if (ranking <= 100 && ranking > 90) {
+        result = "Lendário"
+
+    }
+
+    else if (ranking >= 101) {
+        result = "Imortal"
+
+    }return result
 }
 
-else if (ranking <= 20 && ranking > 10) {
-    nivel = "Bronze"
-
-}
-
-else if (ranking <= 50 && ranking > 20) {
-    nivel = "Prata"
-
-}
-
-else if (ranking <= 80 && ranking > 50) {
-    nivel = "Ouro"
-
-}
-
-else if (ranking <= 90 && ranking > 80) {
-    nivel = "Diamante"
-
-}
-
-else if (ranking <= 100 && ranking > 90) {
-    nivel = "Lendário"
-
-}
-
-else if (ranking >= 101) {
-    nivel = "Imortal"
-
-}
-
-console.log("O Heroi tem saldo de " +ranking + " está no nível " + nivel)
+console.log("O Heroi tem saldo de " + ranking + " está no nível " + nivel)
