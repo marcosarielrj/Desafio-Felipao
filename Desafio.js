cb783aae1e442f1addb16cf6794ed861c24780eb
Marcos Ariel Rodrigues de Jesus <marcosariel.rj@hotmail.com>
2023-11-08 00:20:46 -0300

    Indentar

--- Desafio.js
+++ Desafio.js
@@ -1,48 +1,48 @@
 var nome = "Joao"
 var xp = 3048
-var nivel = "Boi"   
+var nivel = "Boi"
 
 
 
-    if (xp <= 1000){   
-        nivel = "Ferro" 
-        
-    }
+if (xp <= 1000) {
+    nivel = "Ferro"
 
-    else if (xp < 2000 && xp >= 1000){   
-        nivel = "Bronze" 
-        
-    }
+}
 
-    else if (xp < 5000 && xp > 2000){   
-        nivel = "Prata" 
-        
-    }
+else if (xp < 2000 && xp >= 1000) {
+    nivel = "Bronze"
 
-    else if (xp < 7000 && xp > 6000){   
-        nivel = "Ouro" 
-    
-    }
+}
 
-    else if (xp < 8000 && xp > 7000){   
-        nivel = "Platina" 
-        
-    }
+else if (xp < 5000 && xp > 2000) {
+    nivel = "Prata"
 
-    else if (xp < 9000 && xp > 8000){   
-        nivel = "Ascendente" 
-        
-    }
+}
 
-    else if (xp < 10000 && xp > 9000){   
-        nivel = "Imortal" 
-        
-    }
+else if (xp < 7000 && xp > 6000) {
+    nivel = "Ouro"
 
-    else if (xp >= 10001){   
-        nivel = "Prata" 
-        
-    }
+}
+
+else if (xp < 8000 && xp > 7000) {
+    nivel = "Platina"
+
+}
+
+else if (xp < 9000 && xp > 8000) {
+    nivel = "Ascendente"
+
+}
+
+else if (xp < 10000 && xp > 9000) {
+    nivel = "Imortal"
+
+}
+
+else if (xp >= 10001) {
+    nivel = "Prata"
+
+}
 
 
 console.log("O Herói de nome " + nome + " está no nível de " + nivel)
